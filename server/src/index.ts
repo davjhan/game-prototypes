9d81bf693bee45f150fd20931e9b620817ef2349
David Han <davidjhan96@gmail.com>
2022-02-19 17:29:42 -0800

    Websocket using easy-ws

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,48 +1,17 @@
-import { handleRequest } from './handler'
-import { startWebsocket } from './websockets'
+import Router from '@koa/router'
+import Koa, { Context, Middleware } from 'koa'
+import websockify from 'koa-websocket'
+import { enterGame } from './game'
 
-export default {
-  // * request is the same as `event.request` from the service worker format
-  // * waitUntil() and passThroughOnException() are accessible from `ctx` instead of `event` from the service worker format
-  // * env is where bindings like KV namespaces, Durable Object namespaces, Config variables, and Secrets
-  // are exposed, instead of them being placed in global scope.
-  async fetch(req: Request, env, context:ExecutionContext) {
-    const upgradeHeader = req.headers.get("Upgrade")
-    if (upgradeHeader == "websocket") {
-      return startWebsocket(req, env)
-    }else{
-      return handleRequest(req)
-    }
+const app = websockify(new Koa())
+const router = new Router()
 
-  }
-}
+app.ws.use((ctx) => {
+	enterGame(ctx)
+})
 
-export class GameRoom {
-  private state: DurableObjectState
-  private connections: WebSocket[]
-  private gameState: {
-    messages: string[]
-  }
-  constructor(state:DurableObjectState, env) {
-    this.state = state
-    this.gameState = { messages:[] }
-    this.connections = []
-  }
-  async fetch(req:Request) {
-    const body = await req.text()
-    const webSocketPair = new WebSocketPair()
-    const [client, server] = Object.values(webSocketPair)
-    this.connections.push(server)
-    server.accept()
-    server.addEventListener('message', event => {
-      console.log(event.data)
-      this.connections.forEach(it =>{
-        it.send(`New message: ${event.data}`)
-      })
-    })
-    return new Response(undefined, {
-      status: 101,
-      webSocket: client
-    })
-  }
-}
+app.use(router.routes())
+	.use(router.allowedMethods)
+
+app.listen(3000)
+console.log('✨ Server listening on :3000')
